8d12c3665a8f887d573844ef3bdca0a8d2555db6
Oliver Lippert <oliver@lipperts-web.de>
2024-07-24 10:03:30 +0200

    feat: load documents to process by paperless ngx url
    Signed-off-by: Oliver Lippert <oliver@allesit.de>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    print!("Hello world")
+use reqwest::{Client, Error};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Documents {
+    id: u32,
+    content: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct DocumentsResponse {
+    count: u32,
+    results: Vec<Documents>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = format!(
+        "{paperlessngx_url}/api/documents/?tags__id__all={tags_filter}",
+        paperlessngx_url = env!("PAPERLESSNGX_URL"),
+        tags_filter = env!("PAPERLESSNGX_TAGS")
+    );
+    let response = Client::new()
+        .get(&request_url)
+        .header(
+             "Authorization",
+            format!(
+                "Token {token}",
+                token = env!("PAPERLESSNGX_TOKEN")
+            ),
+        )
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        let documents = response.json::<DocumentsResponse>().await?;
+        println!("{:?}", documents);
+        Ok(())
+    } else {
+        println!("{:?}", response);
+        panic!("Couldn't load documents")
+    }
 }
