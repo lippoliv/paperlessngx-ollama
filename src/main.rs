03d7b00c952ea3989b0973614b7131a7bea089e8
Oliver Lippert <oliver@lipperts-web.de>
2024-07-24 11:02:01 +0200

    feat: send documents to ollama to generate a summary
    Signed-off-by: Oliver Lippert <oliver@lipperts-web.de>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use ollama_rs::generation::completion::request::GenerationRequest;
+use ollama_rs::generation::parameters::FormatType;
+use ollama_rs::Ollama;
 use reqwest::{Client, Error};
 use serde::Deserialize;
 
@@ -22,7 +25,52 @@ async fn main() {
         .await
         .unwrap();
 
-    println!("{:?}", documents)
+    generate_documents_summary_via_ollama(documents).await;
+}
+
+async fn generate_documents_summary_via_ollama(documents: Vec<Document>) {
+    for document in documents.iter() {
+        generate_document_summary_via_ollama(document).await;
+    }
+}
+
+async fn generate_document_summary_via_ollama(document: &Document) {
+    let ollama = Ollama::new(env!("OLLAMA_HOST"), env!("OLLAMA_PORT").parse::<u16>().unwrap());
+    let model = "llama3:latest";
+    let prompt = format!(
+        "summarize the following text within 150 characters.\
+        this summary will be used in a document management system to later find the latter.\
+        focus on the start of the text.\
+        ignore address information.\
+        \
+        the summary must be one sentence.\
+        just write the summary.\
+        \
+        if you find an invoice number, mention it.\
+        if it's a reminder for an invoice, mention it.\
+        do not write the sender of the letter.\
+        \
+        format your answer as json where the summary is in the field 'summary'\
+        the summary must be in {lang} language \n\n",
+        lang = env!("OLLAMA_LANGUAGE")
+    );
+
+    let limit = if document.content.len() < 4096 {
+        document.content.len()
+    } else {
+        4096
+    };
+
+    let res = ollama.generate(
+        GenerationRequest::new(
+            model.to_string(),
+            format!("{prompt} {content}", content = document.content[..limit].to_string()),
+        ).format(FormatType::Json)
+    ).await;
+
+    if let Ok(res) = res {
+        println!("{}", res.response);
+    }
 }
 
 async fn load_documents_to_process(paperless_url: String, paperless_token: String, paperless_tags: String) -> Result<Vec<Document>, Error> {
