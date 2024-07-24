d53572021ec0d40792c0a1ef74f61c6c002265cb
Oliver Lippert <oliver@lipperts-web.de>
2024-07-24 10:23:52 +0200

    rename Documents to Document
    it's just one document, not many
    
    Signed-off-by: Oliver Lippert <oliver@allesit.de>

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ use reqwest::{Client, Error};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
-struct Documents {
+struct Document {
     id: u32,
     content: String,
 }
 
 #[derive(Deserialize, Debug)]
 struct DocumentsResponse {
-    results: Vec<Documents>,
+    results: Vec<Document>,
 }
 
 #[tokio::main]
@@ -18,12 +18,14 @@ async fn main() {
         env!("PAPERLESSNGX_URL").to_string(),
         env!("PAPERLESSNGX_TOKEN").to_string(),
         env!("PAPERLESSNGX_TAGS").to_string(),
-    ).await;
+    )
+        .await
+        .unwrap();
 
     println!("{:?}", documents)
 }
 
-async fn load_documents_to_process(paperless_url: String, paperless_token: String, paperless_tags: String) -> Result<Vec<Documents>, Error> {
+async fn load_documents_to_process(paperless_url: String, paperless_token: String, paperless_tags: String) -> Result<Vec<Document>, Error> {
     let request_url = format!(
         "{paperless_url}/api/documents/?tags__id__all={paperless_tags}",
     );
